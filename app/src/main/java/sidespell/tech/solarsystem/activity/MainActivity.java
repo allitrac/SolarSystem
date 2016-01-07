996326cf3ef91909559a509bed5288645f84e893
nuTShELLS <claritacartilla@gmail.com>
2016-01-07 17:37:27 +0800

    Adapters

--- app/src/main/java/sidespell/tech/solarsystem/activity/MainActivity.java
+++ app/src/main/java/sidespell/tech/solarsystem/activity/MainActivity.java
@@ -7,12 +7,19 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Spinner;
 
 import sidespell.tech.solarsystem.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    Spinner spinner = (Spinner) findViewById(R.id.spin);
 
+    ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.planets,android.R.layout.simple_spinner_item);
+
+    public void setAdapter(ArrayAdapter<CharSequence> adapter) {
+        this.adapter = adapter;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,4 +29,16 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
     }
+
+
+
+
+
+
 }
+
+
+
+
+
+//(c)developer.android.com
